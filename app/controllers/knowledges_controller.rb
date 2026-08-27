193fe1758cf0e15b66219d20ccb1459aac24b275
TakuyaYamane <swetakuyayamane@gmail.com>
2026-08-28 03:34:56 +0900

    Add Knowledge creation

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -2,4 +2,24 @@ class KnowledgesController < ApplicationController
   def index
     @knowledges = Knowledge.order(created_at: :desc)
   end
+
+  def new
+    @knowledge = Knowledge.new
+  end
+
+  def create
+    @knowledge = Knowledge.new(knowledge_params)
+
+    if @knowledge.save
+      redirect_to knowledges_path, notice: "ナレッジを登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def knowledge_params
+    params.require(:knowledge).permit(:title, :content, :category)
+  end
 end
--- test/controllers/knowledges_controller_test.rb
+++ test/controllers/knowledges_controller_test.rb
@@ -16,4 +16,52 @@ class KnowledgesControllerTest < ActionDispatch::IntegrationTest
     assert_select "p", text: "カテゴリ: 経費"
     assert_select "p", text: "経費精算はシステムから申請してください。"
   end
+
+  test "should get new" do
+    get new_knowledge_url
+
+    assert_response :success
+    assert_select "h1", "ナレッジ新規登録"
+    assert_select "form[action='#{knowledges_path}'][method='post']"
+    assert_select "input[name='knowledge[title]']"
+    assert_select "input[name='knowledge[category]']"
+    assert_select "textarea[name='knowledge[content]']"
+  end
+
+  test "should create knowledge" do
+    assert_difference("Knowledge.count", 1) do
+      post knowledges_url, params: {
+        knowledge: {
+          title: "VPNへの接続方法",
+          content: "VPNクライアントを起動し、社員アカウントでログインしてください。",
+          category: "IT"
+        }
+      }
+    end
+
+    assert_redirected_to knowledges_url
+    follow_redirect!
+    assert_response :success
+    assert_select "p", text: "ナレッジを登録しました。"
+    assert_select "h2", "VPNへの接続方法"
+  end
+
+  test "should render new with errors when validation fails" do
+    assert_no_difference("Knowledge.count") do
+      post knowledges_url, params: {
+        knowledge: {
+          title: "",
+          content: "",
+          category: ""
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "h1", "ナレッジ新規登録"
+    assert_select "p", text: "入力内容を確認してください。"
+    assert_select "li", text: "Title can't be blank"
+    assert_select "li", text: "Content can't be blank"
+    assert_select "li", text: "Category can't be blank"
+  end
 end
